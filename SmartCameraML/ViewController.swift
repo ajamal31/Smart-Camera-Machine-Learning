8e04e8d333979fc2789fa634304740b40784c841
Arun Jamal <arun.jamal31@gmail.com>
2020-10-31 20:09:59 -0600

    Initial Commit

---
+++ SmartCameraML/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SmartCameraML
+//
+//  Created by Arun Jamal on 2020-10-31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
